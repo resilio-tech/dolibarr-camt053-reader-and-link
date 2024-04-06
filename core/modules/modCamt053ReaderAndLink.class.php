6651e03c9f713e60895f5a5a8b36113587a16e1b
Maximilien Valenzano <slordef.vikamet@gmail.com>
2024-04-06 22:40:37 +0200

    fix(version): v1.2

--- core/modules/modCamt053ReaderAndLink.class.php
+++ core/modules/modCamt053ReaderAndLink.class.php
@@ -72,7 +72,7 @@ class modCamt053ReaderAndLink extends DolibarrModules
 		$this->editor_url = '';
 
 		// Possible values for version are: 'development', 'experimental', 'dolibarr', 'dolibarr_deprecated', 'experimental_deprecated' or a version string like 'x.y.z'
-		$this->version = '1.1';
+		$this->version = '1.2';
 		// Url to the file with your last numberversion of this module
 		//$this->url_last_version = 'http://www.example.com/versionmodule.txt';
 
