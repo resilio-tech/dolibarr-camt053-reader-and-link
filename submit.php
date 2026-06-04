d01c75d8b4e849874671ae4b13db5f7a42619fbf
Maximilien Valenzano <slordef.vikamet@gmail.com>
2026-06-04 10:21:45 +0200

    fix: catch Throwable in submit.php to prevent blank page on PHP 8 errors

--- submit.php
+++ submit.php
@@ -223,11 +223,8 @@ if ($action == 'upload') {
 			$redirectUrl = DOL_URL_ROOT . '/compta/bank/releve.php?account=' . ((int) $firstAccountId) . '&num=' . urlencode($date_concil);
 			setEventMessages($langs->trans('AllEntriesReconciled'), null, 'mesgs');
 		}
-	} catch (Exception $e) {
+	} catch (Throwable $e) {
 		dol_syslog('CAMT053: Error processing file - ' . $e->getMessage(), LOG_ERR);
-		$processError = $e->getMessage();
-	} catch (TypeError $e) {
-		dol_syslog('CAMT053: Type error processing file - ' . $e->getMessage(), LOG_ERR);
 		$processError = $e->getMessage();
 	}
 }
